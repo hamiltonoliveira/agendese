e901c09095b358291a2f870a90abbd027e17dc53
Hamilton Vale <hamilton.valnet@gmail.com>
2024-10-17 15:14:10 -0300

    supaBase insert

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,36 +1,31 @@
 import { Injectable } from '@angular/core';
 import { InitSupaBase } from '../utils/InitSupaBase';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Cliente } from '../models/cliente.model';
 
-import { corsHeaders } from './../../../shared/cors';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
   supabaseUrl = InitSupaBase.supabaseUrl;
-  supabaseKey =   InitSupaBase.supabaseKey;
+  supabaseKey = InitSupaBase.supabaseKey;
+  supabase: any;
 
-
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) {
+  }
 
   addClient(clientData: Cliente): Observable<Cliente> {
     const headers = new HttpHeaders({
-      ...corsHeaders,
-      'Authorization': `apikey ${this.supabaseKey}`,
-      'Content-Type': 'application/json'
+      Authorization: `apikey ${this.supabaseKey}`,
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST, GET, OPTIONS'
     });
 
-    console.log({headers})
-
-    return this.http.post<Cliente>(this.supabaseUrl, clientData, { headers });
+    return this.http.post<Cliente>(this.supabaseUrl,JSON.stringify(clientData),{ headers }
+    );
   }
 
-
-  }
-
-
-
-
+}
